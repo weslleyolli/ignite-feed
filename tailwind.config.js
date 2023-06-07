30d79e5eb4b57205112df7ebd8933087e1482703
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-06-06 23:03:56 -0300

    colors ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green100: '#00B37E',
+        green200: '#00875F',
+
+        gray100: '#E1E1E6',
+        gray200: '#C4C4CC',
+        gray300: '#8D8D99',
+        gray400: '#7C7C8A',
+        gray500: '#323238',
+        gray600: '#202024',
+        gray700: '#121214',
+
+        reddanger: '#F75A68',
+
+        white: '#FFFFFF',
+      }
+    },
   },
   plugins: [],
 }
